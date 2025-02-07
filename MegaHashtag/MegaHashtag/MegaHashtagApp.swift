a37173bd90288e636b89924d805785ad0dba11f3
Matheus Pereira Kulick <matheuskulick@hotmail.com>
2025-02-07 10:43:02 -0300

    [ADD] Xcode project

---
+++ MegaHashtag/MegaHashtag/MegaHashtagApp.swift
@@ -0,0 +1,17 @@
+//
+//  MegaHashtagApp.swift
+//  MegaHashtag
+//
+//  Created by Matheus Pereira Kulick on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MegaHashtagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
